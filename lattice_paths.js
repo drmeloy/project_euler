ed241aac4978b5f6b7c7a590cc6bf48985e5818a
drmeloy <drmeloy@gmail.com>
2020-07-24 16:36:04 -0400

    starting lattice problem

---
+++ lattice_paths.js
@@ -0,0 +1,10 @@
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+// How many such routes are there through a 20×20 grid?
+
+// Note to self: a 2x2 grid is 3x3 in terms of corners
+
+const generateMatrix = num => new Array(num).fill(new Array(num).fill(0));
+const 
+
+
+console.log(generateMatrix(3));

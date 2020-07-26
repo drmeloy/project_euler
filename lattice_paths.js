c7d43ccf3e7c725762c59a45e17a9b7fec5867fa
drmeloy <drmeloy@gmail.com>
2020-07-26 16:39:03 -0400

    attempting recursive solution

--- lattice_paths.js
+++ lattice_paths.js
@@ -1,10 +1,19 @@
 // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 // How many such routes are there through a 20×20 grid?
 
-// Note to self: a 2x2 grid is 3x3 in terms of corners
-
 const generateMatrix = num => new Array(num).fill(new Array(num).fill(0));
-const 
 
+const stepRight = array => array.push('R');
+const stepDown = array => array.push('D');
 
-console.log(generateMatrix(3));
+const takeSteps = (limit, x, y) => {
+  if(x === limit && y === limit) return 2;
+  if(x < limit) return 1 + takeSteps(limit, x + 1, y);
+  if(y < limit) return 1 + takeSteps(limit, x, y + 1);
+}
+
+const findSteps = gridWidth => {
+  return takeSteps(gridWidth, 0, 0, 0)
+};
+
+console.log(findSteps(20));
